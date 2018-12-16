2ac94ba64722b612ea3529fa3261e837eb401645
Max J. Polster <max.polster@id-p.de>
2018-12-16 08:15:02 +0100

    Added additional pipeline tests.

--- test/pipeline.js
+++ test/pipeline.js
@@ -3,6 +3,10 @@
 const test = require('ava')
 const Pipeline = require('..')
 
+test('pipeline (api assertions)', t => {
+	t.throws(() => new Pipeline('foo'))
+})
+
 test('use', async t => {
 	async function foo(ctx) {
 		return 'bar'
@@ -198,6 +202,19 @@ test('dispose silent', async t => {
 	t.false(await pipeline.enable())
 })
 
+test('dispose error', async t => {
+	const pipeline = new Pipeline(async ctx => {
+		ctx.on('dispose', addDisposal => {
+			addDisposal(Promise.reject('foo'))
+		})
+	})
+	await pipeline.enable()
+	pipeline.on('dispose-error', err => {
+		t.is(err, 'foo')
+	})
+	await pipeline.disable()
+})
+
 test('await context disposals', async t => {
 	let disposed = 0
 	let isReady = true
@@ -229,4 +246,98 @@ test('await context disposals', async t => {
 	})
 	t.is(disposed, 1)
 	t.true(isReady)
+})
+
+test('add multiple (and non-) disposals', async t => {
+	let resolved = 0
+	const pipeline = new Pipeline(async ctx => {
+		ctx.on('dispose', addDisposal => {
+			const duplicate = new Promise(resolve => setImmediate(() => {
+				resolved++
+				resolve()
+			}))
+			addDisposal(duplicate)
+			addDisposal(duplicate)
+			addDisposal(new Promise(resolve => setImmediate(() => {
+				resolved++
+				resolve()
+			})))
+			addDisposal('foo')
+		})
+	})
+	await pipeline.enable()
+	await pipeline.disable()
+	t.is(resolved, 2)
+})
+
+test('disable', async t => {
+	t.plan(1)
+	const pipeline = new Pipeline(async ctx => {
+		t.pass()
+		setImmediate(() => ctx.dispose())
+	})
+	await pipeline.enable()
+	pipeline.disable()
+	pipeline.disable()
+})
+
+test('disable (then dispose entry)', async t => {
+	let lastCtx
+	const pipeline = new Pipeline(async ctx => {
+		t.is(lastCtx, undefined)
+		lastCtx = ctx
+		ctx.on('dispose', () => {
+			pipeline.disable()
+		})
+	})
+	await pipeline.enable()
+	lastCtx.dispose()
+	await new Promise(setImmediate)
+})
+
+test('disable (await disposals)', async t => {
+	let disposalResolved = false
+	const pipeline = new Pipeline(async ctx => {
+		ctx.on('dispose', addDisposal => {
+			addDisposal(new Promise(resolve => setImmediate(() => {
+				disposalResolved = true
+				resolve()
+			})))
+		})
+	})
+	await pipeline.enable()
+	t.false(disposalResolved)
+	await pipeline.disable()
+	t.true(disposalResolved)
+})
+
+test('destroy unused', async t => {
+	let state = 0
+	let fooDisposed = false
+	async function foo(ctx) {
+		ctx.on('dispose', () => {
+			fooDisposed = true
+			t.is(state, 2)
+		})
+	}
+	const pipeline = new Pipeline(async ctx => {
+		if (state === 0) {
+			await ctx.use(foo)
+			setImmediate(() => ctx.dispose())
+		} else if (state === 1) {
+			t.false(fooDisposed)
+		} else {
+			t.fail()
+		}
+		state++
+	})
+	pipeline.enable()
+	await new Promise(resolve => {
+		pipeline.on('resolve', () => {
+			if (state === 2) {
+				t.true(fooDisposed)
+				resolve()
+			}
+		})
+	})
 })
